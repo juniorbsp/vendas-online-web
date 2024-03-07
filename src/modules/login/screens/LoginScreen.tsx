add5af03dcd2a614e699c77aa4e1ed2556c69c46
juniorbsp <juniorbsp@hotmail.com>
2024-03-06 23:37:18 -0300

    fear: create module login- responsividade

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,19 @@
-import { BackgroundImage } from '../styles/loginScreen.styles';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  LimitContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.png" />
+      <ContainerLogin>
+        <LimitContainer>
+          <LogoImage src="./logo.png" />
+        </LimitContainer>
+      </ContainerLogin>
     </div>
   );
 };
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -7,4 +7,28 @@ export const BackgroundImage = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: 1;
+`;
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d9d9d9;
+  width: 100%;
+  height: 100vh;
+  max-width: 646px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 2;
+`;
+
+export const LogoImage = styled.img``;
+
+export const LimitContainer = styled.div`
+  width: 100%;
+  max-width: 498px;
+  display: flex;
+  justify-content: center;
 `;
